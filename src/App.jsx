0eeebd39988969b9bd6ea6d8dc9dcaf946243169
Murod1303 <murodshernazaroff@gmail.com>
2024-05-09 22:26:09 +0500

    initial table

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,12 @@ import "./App.css";
 import Home from "./pages/Home/Home";
 import { GlobalStyle } from "./styles/GlobalStyles";
 import ParticlesComponent from "./particles";
-
+import {} from "./theme/theme";
 function App() {
   return (
     <>
       <GlobalStyle />
+
       <Home />
     </>
   );
